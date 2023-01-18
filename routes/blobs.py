df47ed90b1cc64f490c56d60fc872593058da3b6
ImMikeC <hebressus@gmail.com>
2023-01-18 17:47:21 +0000

    Se definen parametros de resize y position de imagen2

--- routes/blobs.py
+++ routes/blobs.py
@@ -29,52 +29,28 @@ def delete_file(container: str = Form(...), filename: str = Form(...)):
     return delete_blob(filename, container)
 
 @blob_routes.post("/overlay")
-async def overlay_images(container: str, file1: str, file2: str, x: int, y: int, output: str):
+async def overlay_images(container: str, file1: str, file2: str, width: int, height: int, x: int, y: int, output: str):
     try:
-        # conexion al Azure Blob storage
+       # conexion al Azure Blob storage en azure_blob_functions/blobs.py
        
+       # blob para imagen 1
         blob_client_file1 = blob_service_client.get_blob_client(container=container, blob=file1)
-        # obtener container
-
-        # blob para imagen 1
-        # Read the first image and save it as a numpy array
-
-        # image1 = cv2.imdecode(
-        #     np.asarray(bytearray(blob_client_file1.download_blob().readall()), dtype=np.uint8),
-        #     cv2.IMREAD_COLOR
-        # )
         image1 = Image.open(io.BytesIO(blob_client_file1.download_blob().readall()))
-
 
         # blob para imagen 2
         blob_client_file2 = blob_service_client.get_blob_client(container=container, blob=file2)
-
-        # Read the second image and save it as a numpy array
-        # image2 = cv2.imdecode(
-        #     np.asarray(bytearray(blob_client_file2.download_blob().readall()), dtype=np.uint8),
-        #     cv2.IMREAD_COLOR
-        # )
         image2 = Image.open(io.BytesIO(blob_client_file2.download_blob().readall()))
+        # Se ajusta tamaño de la imagen de acuerdo al input
+        image2 = image2.resize((height, width))
 
-        # ajusta tamano de la imagen 2 al tamano de la imagen 1
-        # image2 = cv2.resize(image2, (image1.shape[1], image1.shape[0]))
-        # predefine alpha y beta
-        # alpha = 0.5
-        # beta = 1 - alpha
-        
-        # overlay usando cv2.addWeighted()
-        # overlay = cv2.addWeighted(image1, alpha, image2, beta, 0)
-        
-        # comprime imagen antes de subirla al Azure Blob storage
-        # _, img_encoded = cv2.imencode('.jpg', overlay)
-        # img_bytes = img_encoded.tobytes()
-
+        # Se crea imagen con las mismas caracteristicas que la imagen 1.
         result = Image.new(image1.mode, image1.size)
 
         result.paste(image1)
-
+        # x para posicion horizontal e y para posicion vertical
         result.paste(image2, (x,y), mask = image2)
 
+        # Guardar result() como objeto binario
         img_bytes = io.BytesIO()
         result.save(img_bytes, format='JPEG')
         img_bytes.seek(0)
