3edc461811f9b15e019728aa2e6049d9671a3a27
ImMikeC <hebressus@gmail.com>
2023-02-13 08:47:29 +0000

    Se agrega endpoint para OCR.

--- routes/blobs.py
+++ routes/blobs.py
@@ -6,6 +6,18 @@ from PIL import Image
 import io
 from azure_blob_functions.blob import blob_service_client
 
+#importaciones para OCR - Computer Vision
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+
 blob_routes = APIRouter()
 
 @blob_routes.post("/upload")
@@ -18,8 +30,6 @@ async def upload(container: str = Form(...), file: UploadFile = File(...)):
 def get_file(container: str, filename: str):
     return get_blob(filename, container)
 
-
-
 @blob_routes.get("/download/{container}/{filename}")
 def download_file(container: str, filename: str):
     return download_blob(filename, container)
@@ -31,7 +41,7 @@ def delete_file(container: str = Form(...), filename: str = Form(...)):
 @blob_routes.post("/overlay")
 async def overlay_images(container: str, file1: str, file2: str, width: int, height: int, x: int, y: int, output: str):
     try:
-       # conexion al Azure Blob storage en azure_blob_functions/blobs.py
+       # conexion al Azure Blob storage en azure_blob_functions/blob.py
        
        # blob para imagen 1
         blob_client_file1 = blob_service_client.get_blob_client(container=container, blob=file1)
@@ -60,3 +70,35 @@ async def overlay_images(container: str, file1: str, file2: str, width: int, hei
         return {"message": "Overlay image uploaded to Azure Blob storage successfully."}
     except Exception as e:
         return {"error": str(e)}
+
+@blob_routes.post("/ocr")
+async def image_ocr(file: str, ocrOutput: str):
+    subscription_key = '4715df58f9ad49ae8218dd76edc6158b'
+    endpoint = 'https://cs-hs-techlabs-dev-001.cognitiveservices.azure.com/'
+    computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+    read_image_url = 'https://stgehspocdev001.blob.core.windows.net/files-demo/' + file
+    
+    read_response = computervision_client.read(read_image_url,  raw=True, model_version= "latest")
+    read_operation_location = read_response.headers["Operation-Location"]
+    operation_id = read_operation_location.split("/")[-1]
+
+    while True:
+        read_result = computervision_client.get_read_result(operation_id)
+        if read_result.status not in ['notStarted', 'running']:
+            break
+        time.sleep(1)
+
+    if read_result.status == OperationStatusCodes.succeeded:
+        for text_result in read_result.analyze_result.read_results:
+            for line in text_result.lines:            
+                print(line.text, file=open(ocrOutput + '.txt', "a"))
+
+    
+
+            
+                
+    
+    
+
+
